a86debd542f0fd3dd6e6388238f1c0ff5a103e3b
anuj <anujsbhuyar@gmail.com>
2024-09-29 23:09:12 +0530

    deployment fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+// import { defineConfig } from "vite"
+// import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this matches the publish directory in netlify.toml
+  },
+});
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // ... other configurations
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    // ... other configurations
+  };
+  
+  
